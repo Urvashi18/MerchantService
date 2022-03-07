22f46a83e40c0fbf0588d596be1cfdf9419b44cb
urvashi <urvashishah94@gmail.com>
2022-03-08 02:24:20 +0530

    add additional data row for testing

--- merchant-service-backend/docker/db/setup.sql
+++ merchant-service-backend/docker/db/setup.sql
@@ -4,3 +4,4 @@ CREATE TABLE Merchants (id long NOT NULL , name varchar(255) NOT NULL, country v
 INSERT into Merchants Value (1,"Amazon","USA","https://www.amazon.com/","Online & In-store");
 INSERT into Merchants Value (2,"Amc Theatres","USA","https://www.amctheatres.com/","Online & In-store");
 INSERT into Merchants Value (3,"Amazon","Canada","https://www.amazon.ca/","Online & In-store");
+INSERT into Merchants Value (4,"Tim Hortons","Canada","https://www.timhortans.ca/","Online & In-store");
